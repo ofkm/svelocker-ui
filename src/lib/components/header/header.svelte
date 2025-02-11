b14d952f319af535dd2dbd998addc7db9205e100
Kyle Mendell <kmendell@ofkm.us>
2025-02-11 08:48:25 -0600

    chore: cleanup

--- src/lib/components/header/header.svelte
+++ src/lib/components/header/header.svelte
@@ -3,11 +3,10 @@
 
 <div class="w-full border-b">
 	<div
-		class="mx-auto flex w-full items-center justify-between px-4 md:px-10"
-	>
+		class="mx-auto flex w-full items-center justify-between px-4 md:px-10">
     <div class="flex h-16 items-center">
-        <img src="/img/s.png" class="mr-3 h-8 w-8 rounded-xl" alt="Logo" />
-        <h1 class="text-lg font-medium" data-testid="application-name">
+        <img src="/img/s.png" class="mr-3 h-8 w-8" style="border-radius: 50%;" alt="Logo" />
+        <h1 class="text-lg font-medium poppins" data-testid="application-name">
 					Svelocker UI
         </h1>
     </div>
