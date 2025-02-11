046a05273cd78972b6e5b3afd1e65743909ca9d1
Kyle Mendell <kmendell@ofkm.us>
2025-02-11 08:01:56 -0600

    fix: header width , also added tag route

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
     import Header from '$lib/components/header/header.svelte';
   </script>
 
-<div class="relative flex flex-col max-w-[1900px] mx-auto w-full text-sm sm:text-base min-h-screen">
+<div class="relative flex flex-col mx-auto w-full text-sm sm:text-base min-h-screen">
     <Header />
     {@render children()}
 </div>
