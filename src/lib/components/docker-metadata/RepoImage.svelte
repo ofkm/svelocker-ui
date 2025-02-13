d97a9881d4f5755df23ccbd708aba7280cb03815
Kyle Mendell <kmendell@ofkm.us>
2025-02-13 12:37:55 -0600

    chore(fix WIP): setup PageData in RepoImage.svelte

--- src/lib/components/docker-metadata/RepoImage.svelte
+++ src/lib/components/docker-metadata/RepoImage.svelte
@@ -3,16 +3,20 @@
 	import { getDockerTagsNew } from '$lib/utils/tags.ts';
 	import { env } from '$env/dynamic/public'
 	import MetadataDrawer from '$lib/components/docker-metadata/MetadataDrawer.svelte';
+	import type { PageData } from '../../../routes/$types';
 
 	let tagsArray: ImageTag[] = [];
 
 	export let repo;
+	export let data: PageData;
 
 	getDockerTagsNew(env.PUBLIC_REGISTRY_URL, repo)
 		.then((repoImage) => {
 			tagsArray = repoImage.tags;
 		})
 		.catch((error) => console.error('Error fetching repo images:', error));
+
+	console.log(data.repos.repositories);
 </script>
 
 {#each tagsArray as tag}
