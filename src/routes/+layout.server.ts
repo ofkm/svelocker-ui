ca9031458c273d6058c99d469d9267d0564a2ced
Kyle Mendell <kmendell@ofkm.us>
2025-02-13 12:28:07 -0600

    chore(WIP): fix client side typescript calls

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,14 +2,21 @@ import { getRegistryRepos } from '$lib/utils/repos.ts';
 import { env } from '$env/dynamic/public'
 
 export async function load() {
-	// let reposArray = [] as { name: string }[];
-	//
-	// let reposTest = [] as string[];
-
 	// Call the function with the specified URL
 	// getRegistryRepos('https://kmcr.cc/v2/_catalog').then(promise => {reposTest = promise.repositories;}).catch(error => console.error('Error:', error));
 
+	// Any child routes or components nested within the layout will also have access to the data prop.
+	// 	Code
+	//
+	// <!-- src/routes/some-page/+page.svelte -->
+	// <script lang="ts">
+	// import type { PageData } from './$types';
+	//
+	// export let data: PageData;
+
+	const repos = await getRegistryRepos(env.PUBLIC_REGISTRY_URL + '/v2/_catalog')
+
 	return {
-		repos: await getRegistryRepos(env.PUBLIC_REGISTRY_URL + '/v2/_catalog')
+		repos: repos,
 	};
 }
