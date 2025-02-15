6af5c6a41b9bef9030bbf76c21d719a2eab1af38
Kyle Mendell <kmendell@ofkm.us>
2025-02-15 13:46:44 -0600

    fix: text mismatch when only 1 tag was found

--- src/lib/components/RepoCard.svelte
+++ src/lib/components/RepoCard.svelte
@@ -9,7 +9,7 @@
 
 <div class="grid grid-cols-1 md:grid-cols-1 gap-4 p-10">
 	{#each filteredData as repo, index}
-		<CollapsibleCard id={repo.name} title={repo.name} description="{repo.images.length.toString()} Tags Found">
+		<CollapsibleCard id={repo.name} title={repo.name} description="{repo.images.length > 1 ? repo.images.length.toString() + ' Tags Found' : repo.images.length.toString() + ' Tag Found'}">
 			<RepoImage repoIndex={index} filteredData={filteredData} />
 			<div class="clearfix"></div>
 		</CollapsibleCard>
