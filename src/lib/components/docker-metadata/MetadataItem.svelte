29d8daafcf9451f6330f8e2d632f762e3eb2aed0
Kyle Mendell <kmendell@ofkm.us>
2025-02-16 09:55:12 -0600

    refactor: move metadata item to seperate svelte component

---
+++ src/lib/components/docker-metadata/MetadataItem.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Label } from '$lib/components/ui/label';
+	import { type Icon as IconType } from 'lucide-svelte';
+
+	export let icon: IconType;
+	export let label: string;
+	export let value: string;
+</script>
+
+<div>
+	<Label for={label} class="font-light text-muted-foreground flex items-center gap-2 pb-2">
+		<svelte:component this={icon} class="w-5 h-5" />
+		{label}
+	</Label>
+	<p class="text-sm font-semibold pb-2" id={label}>
+		{value}
+	</p>
+</div>
