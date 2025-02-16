56f736edec5cb1fcab9373178f5e7b6b2968f54e
Kyle Mendell <kmendell@ofkm.us>
2025-02-16 11:02:44 -0600

    fix: header text not following css rules

--- src/lib/components/header/header.svelte
+++ src/lib/components/header/header.svelte
@@ -6,7 +6,7 @@
 	<div class="flex w-full items-center justify-between px-4 md:px-10">
 		<div class="flex h-16 items-center">
 			<img src="/img/svelocker.png" class="mr-3 h-8 w-8" style="border-radius: 50%;" alt="Logo" />
-			<h1 class="text-lg font-medium poppins" data-testid="application-name">Svelocker UI</h1>
+			<p class="text-lg font-bold">Svelocker UI</p>
 		</div>
 		<div class="sm:flex items-center gap-4 hidden">
 			<a
