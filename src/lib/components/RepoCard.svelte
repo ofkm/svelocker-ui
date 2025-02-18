f33ed13e8b662c74ec915178fbdf56936698d896
Kyle Mendell <kmendell@ofkm.us>
2025-02-18 12:53:01 -0600

    fix: group repo data to show all in the same card

--- src/lib/components/RepoCard.svelte
+++ src/lib/components/RepoCard.svelte
@@ -4,13 +4,36 @@
 	import type { RegistryRepo } from '$lib/models/repo.ts';
 
 	export let filteredData: RegistryRepo[];
+
+	// Group the data by repo name
+	$: groupedData = filteredData.reduce(
+		(acc, repo) => {
+			const name = repo.name;
+			if (!acc[name]) {
+				acc[name] = [];
+			}
+			acc[name].push(repo);
+			return acc;
+		},
+		{} as Record<string, RegistryRepo[]>
+	);
 </script>
 
 <div class="grid grid-cols-1 md:grid-cols-1 gap-4 p-10">
-	{#each filteredData as repo, index}
+	{#each Object.entries(groupedData) as [repoName, repos]}
+		<CollapsibleCard id={repoName} title={repoName} description={`${repos.length} ${repos.length > 1 ? 'Images' : 'Image'} Found`}>
+			{#each repos as repo, index}
+				<RepoImage repoIndex={index} filteredData={[repo]} />
+			{/each}
+			<div class="clearfix"></div>
+		</CollapsibleCard>
+	{/each}
+
+	<!-- Old Version -->
+	<!-- {#each filteredData as repo, index}
 		<CollapsibleCard id={repo.name} title={repo.name} description={repo.images.length > 1 ? repo.images.length.toString() + ' Images Found' : repo.images.length.toString() + ' Image Found'}>
 			<RepoImage repoIndex={index} {filteredData} />
 			<div class="clearfix"></div>
 		</CollapsibleCard>
-	{/each}
+	{/each} -->
 </div>
