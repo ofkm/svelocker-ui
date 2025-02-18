189275d90fe62f06ca7a8204c0f1471382a8c088
Kyle Mendell <kmendell@ofkm.us>
2025-02-17 21:51:13 -0600

    fix: add github icon next to version label

--- src/lib/components/header/header.svelte
+++ src/lib/components/header/header.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { version as currentVersion } from '$app/environment';
+	import { Github } from 'lucide-svelte';
 </script>
 
 <header class="w-full border-b flex">
@@ -9,7 +10,8 @@
 			<p class="text-lg font-bold">Svelocker UI</p>
 		</div>
 		<div class="sm:flex items-center gap-4 hidden">
-			<a href="https://github.com/kmendell/svelocker-ui" class="duration-200 font-bold text-muted-foreground hover:text-blue-400 no-underline" target={'_blank'}>
+			<a href="https://github.com/kmendell/svelocker-ui" class="duration-200 font-bold text-muted-foreground hover:text-blue-400 no-underline flex items-center gap-2" target={'_blank'}>
+				<Github size={20} />
 				<p class="">v{currentVersion}</p>
 			</a>
 		</div>
