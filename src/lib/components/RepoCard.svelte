18ccc34589bce382a453398ab51a1f67306c8a1f
Kyle Mendell <kmendell@ofkm.us>
2025-02-18 12:39:33 -0600

    chore: add table packages

--- src/lib/components/RepoCard.svelte
+++ src/lib/components/RepoCard.svelte
@@ -4,11 +4,15 @@
 	import type { RegistryRepo } from '$lib/models/repo.ts';
 
 	export let filteredData: RegistryRepo[];
+
+	function getNamespace(fullName: string): string {
+		return fullName.split('/')[0];
+	}
 </script>
 
 <div class="grid grid-cols-1 md:grid-cols-1 gap-4 p-10">
 	{#each filteredData as repo, index}
-		<CollapsibleCard id={repo.name} title={repo.name} description={repo.images.length > 1 ? repo.images.length.toString() + ' Tags Found' : repo.images.length.toString() + ' Tag Found'}>
+		<CollapsibleCard id={getNamespace(repo.name)} title={getNamespace(repo.name)} description={repo.images.length > 1 ? repo.images.length.toString() + ' Tags Found' : repo.images.length.toString() + ' Tag Found'}>
 			<RepoImage repoIndex={index} {filteredData} />
 			<div class="clearfix"></div>
 		</CollapsibleCard>
