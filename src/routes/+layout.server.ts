8405b3c1dc1be92da2c1551a296fc90cd207b480
Kyle Mendell <kmendell@ofkm.us>
2025-02-19 17:40:00 -0600

    chore: removed un-used code

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -131,18 +131,4 @@ export async function load({ url }) {
 			};
 		}
 	}
-
-	// try {
-	// 	const repos = await getRegistryReposAxios(env.PUBLIC_REGISTRY_URL + '/v2/_catalog');
-	// 	return {
-	// 		repos: repos,
-	// 		error: null
-	// 	};
-	// } catch (error) {
-	// 	console.error('Failed to connect to registry:', error);
-	// 	return {
-	// 		repos: { repositories: [] },
-	// 		error: 'Failed to connect to registry'
-	// 	};
-	// }
 }
