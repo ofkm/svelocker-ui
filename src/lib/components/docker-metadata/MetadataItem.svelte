9a411a9a61809a24bede272cfffe3ca6f57badeb
Kyle Mendell <kmendell@ofkm.us>
2025-02-25 21:26:19 -0600

    fix: change unknown text to red

--- src/lib/components/docker-metadata/MetadataItem.svelte
+++ src/lib/components/docker-metadata/MetadataItem.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
 	import { Label } from '$lib/components/ui/label';
 	import type { ComponentType } from 'svelte';
+	import { cn } from '$lib/utils';
 
 	export let icon: ComponentType;
 	export let label: string;
 	export let value: string;
+
+	// Check if the value contains "Unknown"
+	$: isUnknown = value?.includes('Unknown');
 </script>
 
 <div>
@@ -12,7 +16,7 @@
 		<svelte:component this={icon} class="w-5 h-5" />
 		{label}
 	</Label>
-	<p class="text-sm font-semibold pb-2" id={label}>
+	<p class={cn('text-sm font-semibold pb-2', isUnknown ? 'text-destructive' : '')} id={label}>
 		{value}
 	</p>
 </div>
