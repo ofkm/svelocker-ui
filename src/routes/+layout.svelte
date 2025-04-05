c88256e887c0d9be68cb36d0579f5e2840cd5a8c
Kyle Mendell <kmendell@ofkm.us>
2025-04-04 19:15:13 -0500

    remove unused page fronm layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,8 @@
 	import Header from '$lib/components/header/header.svelte';
 	import { Toaster } from '$lib/components/ui/sonner/index.js';
 	import { ModeWatcher } from 'mode-watcher';
-	import { page } from '$app/stores';
-	let { children, data } = $props();
 
-	// Get registry name for title (if available)
-	let registryName = $page.data.settings?.registry_name || 'Docker Registry';
+	let { children, data } = $props();
 </script>
 
 <div class="relative flex flex-col w-full text-sm sm:text-base min-h-screen max-h-full">
