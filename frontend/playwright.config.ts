b7cfd10eea47a254c1a91a5ff443f082b7e684d1
Kyle Mendell <kmendell@ofkm.us>
2025-04-09 15:44:14 -0400

    chore: fix some playwright configs

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
 	outputDir: './tests/.output',
@@ -10,6 +10,7 @@ export default defineConfig({
 	fullyParallel: false,
 	globalSetup: './tests/e2e/global-setup',
 	globalTeardown: './tests/e2e/global-teardown',
+	reporter: process.env.CI ? [['html', { outputFolder: 'tests/.output/.report' }], ['github']] : [['line'], ['html', { open: 'never', outputFolder: 'tests/.report' }]],
 	use: {
 		baseURL: 'http://localhost:3000',
 		trace: 'on-first-retry'
